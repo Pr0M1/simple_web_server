a52abf2933253dffd5c55781e96c71602a7325c9
Tenke-Dombi Daniel <xl4t3r@gmail.com>
2024-12-12 14:09:35 +0100

    Filled out server.py

--- server.py
+++ server.py
@@ -2,7 +2,23 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
 class SimpleHandler(BaseHTTPRequestHandler):
+    
     def do_get(self):
+        
+        # Response code
+        self.send_response(200) # 200 means "OK"
+
+        # Define response headers
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+
+        # Response body
+        self.wfile.write(b"Hello, World!")
+
 
 host = "localhost"
 port = 8080
+server = HTTPServer((host, port), SimpleHandler)
+
+print(f"Server is running on http://{host}:{port}")
+server.serve_forever
